77c0305f166c4954da718902ff544f07a6c4230d
Samuel Forrest <samueljforrest@outlook.com>
2024-09-14 16:33:14 +0100

    working commit norm

--- constants/index.ts
+++ constants/index.ts
@@ -76,7 +76,7 @@ export const onboarding = [
         id: 1,
         title: "The perfect ride is just a tap away!",
         description:
-            "Your journey begins with Ryde. Find your ideal ride effortlessly.",
+            "Your journey begins with Ryde. Find your ideal ride effortlessly. Powered by React Native, TypeScript, Expo and written by Samuel Forrest.",
         image: images.onboarding1,
     },
     {
